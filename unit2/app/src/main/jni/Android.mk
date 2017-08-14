38fac6014450b2a6ac03011dab2ae92bb5613ff9
309464462 <309464462@qq.com>
2017-08-15 00:53:44 +0800

    这个Android.mk有些不要必须的，之些踩坑可能就会写得有些重复。所以看需要各位进行调整 TARGET_ARCH_ABI := armeabi-v7a 最好指定一下平台，有时候ndk prebuild的时候，有可能会编译全平台，就会出现找不到对应的库的问题的了 LOCAL_STATIC_LIBRARIES := avfilter avformat avcodec postproc swresample swscale avutil x264 这个是非常重要的，后面的那些库的顺序是有讲究的，如果顺序有问题，有可能会报找不到链接的问题 因为FFmpeg每个版本都可能不一样的，所以想要知道这个的顺序，可以去FFmpeg目录下的Makefile文件看看

--- unit2/app/src/main/jni/Android.mk
+++ unit2/app/src/main/jni/Android.mk
@@ -1,17 +1,3 @@
-# Copyright (C) 2009 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
 LOCAL_PATH := $(call my-dir)
 
 # prepare libX
@@ -46,6 +32,13 @@ LOCAL_MODULE    := avutil
 LOCAL_SRC_FILES := libavutil.a
 include $(PREBUILT_STATIC_LIBRARY)
 
+# prepare libX
+include $(CLEAR_VARS)
+TARGET_ARCH_ABI := armeabi-v7a
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+LOCAL_MODULE    := postproc
+LOCAL_SRC_FILES := libpostproc.a
+include $(PREBUILT_STATIC_LIBRARY)
 
 # prepare libX
 include $(CLEAR_VARS)
@@ -80,7 +73,7 @@ LOCAL_SRC_FILES  := live_jni.c
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include
 LOCAL_CFLAGS     := -D__STDC_CONSTANT_MACROS -Wno-sign-compare -Wno-switch -Wno-pointer-sign -DHAVE_NEON=1 -mfpu=neon -mfloat-abi=softfp -fPIC -DANDROID
 
-LOCAL_STATIC_LIBRARIES := avfilter avformat avcodec swresample swscale avutil x264
+LOCAL_STATIC_LIBRARIES := avfilter avformat avcodec postproc swresample swscale avutil x264
 LOCAL_LDLIBS     := -L$(NDK_ROOT)/platforms/$(APP_PLATFORM)/arch-arm/usr/lib -L$(LOCAL_PATH) -llog -ljnigraphics -lz -ldl
 
 
