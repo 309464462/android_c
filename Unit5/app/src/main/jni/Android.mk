8ffdd92ca568e77f08c9214d7778e18e893c410a
309464462 <309464462@qq.com>
2017-08-30 11:52:52 +0800

    添加新的日记框架

--- Unit5/app/src/main/jni/Android.mk
+++ Unit5/app/src/main/jni/Android.mk
@@ -11,6 +11,20 @@ LOCAL_SRC_FILES := src/native.c
 
 LOCAL_MODULE    := native
 
+MY_LOG_TAG := \"logtest"
+
+# 定义基于构建类型的默认日志等级
+ifeq ($(APP_OPTIIM),release)
+  MY_LOG_LEVEL := MY_LOG_LEVEL_ERROR
+else
+  MY_LOG_LEVEL := MY_LOG_LEVEL_VERBOSE
+endif
+
+# 追加编译标志
+#LOCAL_CFLAGS += -DMY_LOG_TAG=$(MY_LOG_TAG)
+#LOCAL_CFLAGS += -DMY_LOG_LEVEL=$(MY_LOG_LEVEL)
+
+
 LOCAL_LDLIBS := -llog
 
 
--- Unit5/app/src/main/jni/src/native.c
+++ Unit5/app/src/main/jni/src/native.c
@@ -2,24 +2,14 @@
 // Created by elvin on 2017/8/29.
 //
 #include "com_example_elvin_unit5_jni_TestJni.h"
-#include <android/log.h>
-#define LOG_TAG "logtest"
-#define LOGI(...) __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
-#define LOGT(...) __android_log_print(ANDROID_LOG_INFO,"alert",__VA_ARGS__)
-#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
-#define LOGW(...) __android_log_print(ANDROID_LOG_WARN,LOG_TAG,__VA_ARGS__)
-#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG,LOG_TAG,__VA_ARGS__)
-
-
-
-
+#include "my_log.h"
 
 JNIEXPORT void JNICALL Java_com_example_elvin_unit5_jni_TestJni_startToShowLog
         (JNIEnv *env, jobject job){
-//    LOGI("Failed to find class");
+    MY_LOG_VERBOSE("test the logcat error ");
 //    __android_log_write(ANDROID_LOG_VERBOSE,"testlog","simple log out");
 //    my_log_verbose("log level is %d",101);
-     __android_log_assert("0 != errno","testlog","there is an error");
+//     __android_log_assert("0 != errno","testlog","there is an error");
 
 }
 
