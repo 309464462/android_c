c9b7939cfcc7a607e05b55105b55200ad3ad6061
309464462 <309464462@qq.com>
2017-08-31 16:15:16 +0800

    开启管道

--- Unit5/app/src/main/jni/src/native.c
+++ Unit5/app/src/main/jni/src/native.c
@@ -4,18 +4,37 @@
 #include "com_example_elvin_unit5_jni_TestJni.h"
 #include "my_log.h"
 #include <stdlib.h>
+#include<stdio.h>
 
 
 JNIEXPORT void JNICALL Java_com_example_elvin_unit5_jni_TestJni_startToShowLog
         (JNIEnv *env, jobject job){
     int result ;
     //与进程交互的过程
-    result = system("mkdir  /data/data/com.example.elvin.unit5/tmp");
-    if(result == -1 || 127 == result){
-        MY_LOG_VERBOSE("mkdir failed............... ");
+//    result = system("mkdir  /data/data/com.example.elvin.unit5/tmp");
+//    if(result == -1 || 127 == result){
+//        MY_LOG_VERBOSE("mkdir failed............... ");
+//    }else{
+//        MY_LOG_VERBOSE("mkdir success ...................");
+//    }
+    //与子进程通信 即通过管道 为父进程 和子进程之间打开一个双向通道
+    FILE * stream;
+    stream = popen("ls","r");
+    if(NULL == stream){
+        MY_LOG_VERBOSE("管道没有创建成功................");
     }else{
-        MY_LOG_VERBOSE("mkdir success ...................");
+        char buffer[1024];
+        int status;
+
+        while(NULL != fgets(buffer,1024,stream)){
+            MY_LOG_VERBOSE("stream:%s",buffer);
+        }
+
+        //关闭通道
+        status = pclose(stream);
+        MY_LOG_VERBOSE("关闭管道：%d",status);
     }
+
 
 //    __android_log_write(ANDROID_LOG_VERBOSE,"testlog","simple log out");
 //    my_log_verbose("log level is %d",101);
