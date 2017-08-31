46e440ddcfe016d41ff8cb97b7583feece2b061a
309464462 <309464462@qq.com>
2017-08-31 19:06:55 +0800

    一直出现“ java.lang.NoSuchMethodError: no static or non-static method” 其实是 参数没写对，没加上括号

--- Unit7/app/src/main/jni/src/native.c
+++ Unit7/app/src/main/jni/src/native.c
@@ -4,6 +4,12 @@
 
 #include "com_example_elvin_unit7_jni_JniOperatiton.h"
 #include "my_log.h"
+#include  <stdio.h>
+#include  <stdlib.h>
+#include <unistd.h>
+
+static jmethodID gOnNativeMesssage = NULL;
+
 /*
  * Class:     com_example_elvin_unit7_jni_JniOperatiton
  * Method:    nativeInt
@@ -11,7 +17,21 @@
  */
 JNIEXPORT void JNICALL Java_com_example_elvin_unit7_jni_JniOperatiton_nativeInt
   (JNIEnv *env, jobject obj){
+      if(NULL == gOnNativeMesssage){
+         jclass  clazz = (*env)->GetObjectClass(env,obj);
+          //参数的括号很重要
+        gOnNativeMesssage = (*env)->GetMethodID(env,clazz,"onNativeMessage","(Ljava/lang/String;)V");
 
+        //抛出异常
+        if(NULL == gOnNativeMesssage){
+            MY_LOG_INFO("no find the method.................");
+            //寻找类，可以现在代码中查询确定
+          jclass  execptionClazz = (*env)->FindClass(env,"java/lang/RuntimeException");
+          (*env)->ThrowNew(env,execptionClazz,"Unable to find method");
+        }else{
+          MY_LOG_INFO("find method.................");
+        }
+      }
   }
 
 /*
@@ -30,5 +50,23 @@ JNIEXPORT void JNICALL Java_com_example_elvin_unit7_jni_JniOperatiton_nativeFree
  * Signature: (I)V
  */
 JNIEXPORT void JNICALL Java_com_example_elvin_unit7_jni_JniOperatiton_nativeWorker
-  (JNIEnv *env, jobject obj, jint  num){
+  (JNIEnv *env, jobject obj, jint id,jint iterrations){
+        //循环给定的次数
+     for(int i = 0; i < iterrations;i++){
+         //准备消息
+         char message[26];
+         sprintf(message,"Worker %d: Iteration %d",id,iterrations);
+
+         //来自c字符串的消息
+         jstring  messageString = (*env)->NewStringUTF(env,message);
+
+         //调用原生消息方法
+         (*env)->CallVoidMethod(env,obj,gOnNativeMesssage,messageString);
+         //检查是否产生异常
+         if(NULL != (*env)->ExceptionOccurred(env)){
+             break;
+         }
+         //睡眠一秒钟
+         sleep(1);
+     }
   }
