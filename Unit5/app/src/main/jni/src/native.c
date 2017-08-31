c83e8fe13dcd1487f9ac610ac18e623ed7e67c19
309464462 <309464462@qq.com>
2017-08-31 16:26:53 +0800

    获取原生信息

--- Unit5/app/src/main/jni/src/native.c
+++ Unit5/app/src/main/jni/src/native.c
@@ -4,35 +4,60 @@
 #include "com_example_elvin_unit5_jni_TestJni.h"
 #include "my_log.h"
 #include <stdlib.h>
-#include<stdio.h>
+#include  <stdio.h>
+#include <unistd.h>
 
-
+void getUIdAndTid();
 JNIEXPORT void JNICALL Java_com_example_elvin_unit5_jni_TestJni_startToShowLog
         (JNIEnv *env, jobject job){
-    int result ;
-    //与进程交互的过程
-//    result = system("mkdir  /data/data/com.example.elvin.unit5/tmp");
-//    if(result == -1 || 127 == result){
-//        MY_LOG_VERBOSE("mkdir failed............... ");
-//    }else{
-//        MY_LOG_VERBOSE("mkdir success ...................");
-//    }
-    //与子进程通信 即通过管道 为父进程 和子进程之间打开一个双向通道
-    FILE * stream;
-    stream = popen("ls","r");
-    if(NULL == stream){
-        MY_LOG_VERBOSE("管道没有创建成功................");
+
+    getUIdAndTid();
+}
+
+
+  void getUIdAndTid(){
+      uid_t uid;
+      uid = getuid();
+      MY_LOG_VERBOSE("Applicaton user id is %u...",uid);
+
+      gid_t  gid;
+      gid = getgid();
+      MY_LOG_VERBOSE("Applicaton user group id is %u...",gid);
+      //获取应用的名字
+      char * username;
+
+      username = getlogin();
+      MY_LOG_VERBOSE("Applicaton user name is %s...",username);
+  }
+
+   void  testSubThread(){
+       int result ;
+       //与进程交互的过程
+    result = system("mkdir  /data/data/com.example.elvin.unit5/tmp");
+    if(result == -1 || 127 == result){
+        MY_LOG_VERBOSE("mkdir failed............... ");
     }else{
-        char buffer[1024];
-        int status;
+        MY_LOG_VERBOSE("mkdir success ...................");
+    }
+   }
 
-        while(NULL != fgets(buffer,1024,stream)){
-            MY_LOG_VERBOSE("stream:%s",buffer);
-        }
+    void tsetSubThreadThroughPipo(){
+        //与子进程通信 即通过管道 为父进程 和子进程之间打开一个双向通道
+        FILE * stream;
+        stream = popen("ls","r");
+        if(NULL == stream){
+            MY_LOG_VERBOSE("管道没有创建成功................");
+        }else{
+            char buffer[1024];
+            int status;
 
-        //关闭通道
-        status = pclose(stream);
-        MY_LOG_VERBOSE("关闭管道：%d",status);
+            while(NULL != fgets(buffer,1024,stream)){
+                MY_LOG_VERBOSE("stream:%s",buffer);
+            }
+
+            //关闭通道
+            status = pclose(stream);
+            MY_LOG_VERBOSE("关闭管道：%d",status);
     }
 
 
