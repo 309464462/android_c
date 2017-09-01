5aa37fe1457e660dde2555b1c529560764811756
309464462 <309464462@qq.com>
2017-09-01 15:04:47 +0800

    使用c++文件，以为要加上LOCAL_CPP_FEATURES += rtti，加上反而有问题了，取消就没事了

--- Unit7/app/src/main/jni/Android.mk
+++ Unit7/app/src/main/jni/Android.mk
@@ -6,7 +6,7 @@ include $(CLEAR_VARS)
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/include  \
                     $(LOCAL_PATH)/src
 
-LOCAL_SRC_FILES := src/native.c
+LOCAL_SRC_FILES := src/thread.cpp
 
 
 LOCAL_MODULE    := native
@@ -20,9 +20,12 @@ else
   MY_LOG_LEVEL := MY_LOG_LEVEL_VERBOSE
 endif
 
+
+LOCAL_CFLAGS := -D__STDINT_LIMITS -DINDEPENDENT_NODE -pipe -fstrict-aliasing -pthread -fexceptions -fpermissive -fno-inline  -frtti -Wdeprecated-declarations
 # 追加编译标志
-#LOCAL_CFLAGS += -DMY_LOG_TAG=$(MY_LOG_TAG)
-#LOCAL_CFLAGS += -DMY_LOG_LEVEL=$(MY_LOG_LEVEL)
+LOCAL_CFLAGS += -DMY_LOG_TAG=$(MY_LOG_TAG)
+LOCAL_CFLAGS += -DMY_LOG_LEVEL=$(MY_LOG_LEVEL)
+#LOCAL_CPP_FEATURES += rtti
 
 LOCAL_PROGUARD_ENABLED:= disabled
 
