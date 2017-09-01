61afed86e76ad958f7ebde620e3288ef74929598
309464462 <309464462@qq.com>
2017-09-01 15:03:03 +0800

    一直报Fatal signal 6 (SIGABRT), code -6 in tid 31866

--- Unit7/app/src/main/jni/src/native.c
+++ Unit7/app/src/main/jni/src/native.c
@@ -24,12 +24,22 @@ static jobject gObj = NULL;
 
 static JNIEnv *gEnv = NULL;
 
+static pthread_mutex_t mutex;
+
 /*
  * Class:     com_example_elvin_unit7_jni_JniOperatiton
  * Method:    nativeInt
  * Signature: ()V
  */
 JNIEXPORT void JNICALL FUN(nativeInt)(JNIEnv *env, jobject obj){
+
+    //初始化互斥量
+    if (0 != pthread_mutex_init(&mutex, NULL)) {
+        //异常
+        jclass exceptionClazz = (*env)->FindClass(env,"java/lang/RuntimeException");
+        //抛出
+        (*env)->ThrowNew(env,exceptionClazz, "Unable to init mutex--");
+    }
         //如果对象的全局引用没有设置
         if(NULL == gObj){
             gObj = (*env)->NewGlobalRef(env,obj);
@@ -73,6 +83,14 @@ JNIEXPORT void JNICALL FUN(nativeFree)(JNIEnv *env, jobject obj){
         gObj = NULL;
     }
 
+    //释放互斥量
+    if (0 != pthread_mutex_destroy(&mutex)) {
+        //异常
+        jclass exceptionClazz = (*env)->FindClass(env,"java/lang/RuntimeException");
+        //抛出
+        (*env)->ThrowNew(env,exceptionClazz, "Unable to destroy mutex--");
+    }
+
   }
 
 
@@ -82,11 +100,21 @@ JNIEXPORT void JNICALL FUN(nativeFree)(JNIEnv *env, jobject obj){
  * Signature: (I)V
  */
 JNIEXPORT void JNICALL FUN(nativeWorker)(JNIEnv *env, jobject obj, jint id,jint iterrations){
-        //循环给定的次数
+
+    //lock
+    if (0 != pthread_mutex_lock(&mutex)) {
+        //异常
+        jclass exceptionClazz = (*env)->FindClass(env,"java/lang/RuntimeException");
+        //抛出
+        (*env)->ThrowNew(env,exceptionClazz, "Unable to lock mutex--");
+        return;
+    }
+
+    //循环给定的次数
      for(int i = 0; i < iterrations;i++){
          //准备消息
          char message[26];
-         sprintf(message,"Worker %d: Iteration %d",id,i);
+         sprintf(message,"nativeWorker Worker %d: Iteration %d",id,i);
 
          //来自c字符串的消息
          jstring  messageString = (*env)->NewStringUTF(env,message);
@@ -101,6 +129,14 @@ JNIEXPORT void JNICALL FUN(nativeWorker)(JNIEnv *env, jobject obj, jint id,jint 
          sleep(1);
 
      }
+
+    //unlock
+    if (0 != pthread_mutex_unlock(&mutex)) {
+        //异常
+        jclass exceptionClazz = (*env)->FindClass(env,"java/lang/RuntimeException");
+        //抛出
+        (*env)->ThrowNew(env,exceptionClazz, "Unable to unlock mutex--");
+    }
   }
 
 /**
@@ -133,7 +169,6 @@ jint JNI_OnLoad(JavaVM* vm, void* reserved) {
     return JNI_VERSION_1_6;
 }
 
-
 /*
  * 由于posix线程不是java平台的一部分，因此虚拟机不能识别它们，为了和java空间交互，posix线程应该先把自己依附到虚拟机上。
  * Class:     com_example_elvin_unit7_jni_JniOperatiton
@@ -142,20 +177,47 @@ jint JNI_OnLoad(JavaVM* vm, void* reserved) {
  */
 JNIEXPORT void JNICALL FUN(posixThreads)(JNIEnv *env, jobject obj, jint threads, jint iteration){
     //为每一个worker创建一个posix线程
+    //获取每个原生线程的句柄
+    pthread_t* handles =malloc(sizeof(pthread_t)*threads);
     for(jint i = 0 ; i < threads;i++){
         //原生 worker 线程参数
         struct NativeWorkerArgs * nativeWorkerArgs = malloc(sizeof(struct NativeWorkerArgs));
         nativeWorkerArgs->id = i;
         nativeWorkerArgs->iterations = iteration;
-        //线程句柄
-        pthread_t thread;
         //创建一个新线程
-        int result= pthread_create(&thread,NULL,nativeWorkerThread,(void *)nativeWorkerArgs);
-
+        int result= pthread_create(&handles[i],NULL,nativeWorkerThread,(void *)nativeWorkerArgs);
         if(0 != result){
             //捕获异常
             jclass  execptionClazz = (*env)->FindClass(env,"java/lang/RuntimeException");
             (*env)->ThrowNew(env,execptionClazz,"Unable to create thread");
+            goto exit;
         }
     }
+    //等待线程终止
+//    for(int i = 0 ; i < threads ; i++){
+//        void * result = NULL;
+//
+//        //它将挂起目标线程的执行，知道目标线程终止
+//        if(0 !=  pthread_join(handles[i],&result)){
+//            //获取异常类
+//            jclass  execptionClazz = (*env)->FindClass(env,"java/lang/RuntimeException");
+//            (*env)->ThrowNew(env,execptionClazz,"Unable to join thread");
+//        }else{
+//            //准备message
+//            char message[26];
+//            sprintf(message,"thread join Worker %d returned %d",i,result);
+//            //来自c字符创的message
+//            jstring meesageString = (*env)->NewStringUTF(env,message);
+//            //调用原生消息
+//            //调用原生消息方法
+//            (*env)->CallVoidMethod(env,obj,gOnNativeMesssage,meesageString);
+//            //检查是否产生异常
+//            if(NULL != (*env)->ExceptionOccurred(env)){
+//               goto  exit;
+//            }
+//        }
+//    }
+    exit:
+        free(handles);
+        return;
 }
